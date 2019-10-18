61cfcfe45c45261d310ab3d607bb94c58ba49a8c
1br9089t <56290155+1br9089t@users.noreply.github.com>
2019-10-18 23:08:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,76 @@
-const version = "0.6.18";
-const cacheName = `airhorner-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html`,
-        `./css/style.css`,
-        `./js/main.js`,
-        `./js/sw.js`,
-        `Unit6.html`,
-      ])
-          .then(() => self.skipWaiting());
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
+
+// キャッシュするファイルをセットする
+const urlsToCache = [
+  '.',
+  'css/style.css',
+  'js/jquery.min.js',
+  'js/main.js',
+  'Unit6.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    // キャッシュを開く
+    caches.open(CACHE_NAME)
+    .then((cache) => {
+      // 指定されたファイルをキャッシュに追加する
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return cacheNames.filter((cacheName) => {
+        // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
+        return cacheName.startsWith(`${registration.scope}!`) &&
+               cacheName !== CACHE_NAME;
+      });
+    }).then((cachesToDelete) => {
+      return Promise.all(cachesToDelete.map((cacheName) => {
+        // いらないキャッシュを削除する
+        return caches.delete(cacheName);
+      }));
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+    .then((response) => {
+      // キャッシュ内に該当レスポンスがあれば、それを返す
+      if (response) {
+        return response;
+      }
+
+      // 重要：リクエストを clone する。リクエストは Stream なので
+      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+      // 必要なので、リクエストは clone しないといけない
+      let fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest)
+        .then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            // キャッシュする必要のないタイプのレスポンスならそのまま返す
+            return response;
+          }
+
+          // 重要：レスポンスを clone する。レスポンスは Stream で
+          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+          // 2つの Stream があるようにする
+          let responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
     })
   );
 });
